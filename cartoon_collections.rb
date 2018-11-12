d5215cd636e5f7e7b15faacbaba4634213f6ab1c
Meisuleen Kased <meisuleen@gmail.com>
2018-11-12 02:27:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,4 +26,9 @@ end
 def find_the_cheese(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheddar_cheese.include?
+  true
+else
+  nil
+end
 end
