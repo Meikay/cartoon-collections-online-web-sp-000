57358bf24eab319f684ae48e25c7d4bfd6df4d9e
Meisuleen Kased <meisuleen@gmail.com>
2018-11-12 01:48:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)# code an argument here
-  dwarves.each_with_index(1) do |person, index|
+  dwarves.each_with_index do |person, index|
    puts "#{index}: #{person}"
  end
 end
