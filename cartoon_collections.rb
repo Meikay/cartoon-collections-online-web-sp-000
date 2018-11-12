0015f573b80e61d40484aba34d8016a1e75b74c7
Meisuleen Kased <meisuleen@gmail.com>
2018-11-12 02:47:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_the_cheese(cheese_types)
     cheese_types = ["cheddar", "gouda", "camembert"]
     if cheese_types.include?("cheddar")
       "cheddar"
-    elsif !cheese_types.include?(cheese_types)
+    elsif find_the_cheese(no_cheese)
       nil
     end
 end
