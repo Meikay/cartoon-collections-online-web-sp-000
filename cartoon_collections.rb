8be28076528084aff928d08606ab2bb503a5f591
Meisuleen Kased <meisuleen@gmail.com>
2018-11-12 03:23:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,8 +26,7 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.include? do |cheese|
+  food.find.include? do |cheese|
   "#{cheese.first}"
-
   end
 end
