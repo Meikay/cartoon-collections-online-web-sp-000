4e7b1f72876c34ea09de454317776133e8efac33
Meisuleen Kased <meisuleen@gmail.com>
2018-11-12 02:37:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,7 @@ end
 def find_the_cheese(cheese_types)
   # the array below is here to help
     cheese_types = ["cheddar", "gouda", "camembert"]
+    binding.pry
     if cheese_types.include?("cheddar")
     puts "#{cheese_types}"
   end
