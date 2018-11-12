6164558b69e4870abe5c010e95e1cde93626f92d
Meisuleen Kased <meisuleen@gmail.com>
2018-11-12 02:28:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,8 +26,9 @@ end
 def find_the_cheese(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.include?("cheddar")
+  if cheddar_cheese.include?("cheddar")
   true
 else
   nil
+end
 end
