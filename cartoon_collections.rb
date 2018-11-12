f032e4f6122f8e93f87540bb16ac18e2651b3afe
Meisuleen Kased <meisuleen@gmail.com>
2018-11-12 03:15:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,9 +25,8 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(food)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   food.find do |cheese|
-  "#{cheese_types}"
-end
+  "#{cheese}"
+  end
 end
